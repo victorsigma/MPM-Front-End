628417dd5e456036bab0f333a7974b4179b51ff6
victorsigma <victorossielgaray1@gmail.com>
2022-07-10 18:32:23 -0500

    Agregar Miembros

--- src/app/componets/project-members/project-members.component.ts
+++ src/app/componets/project-members/project-members.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { UsersProjectsService } from '../../services/users-projects.service';
 import { ProjectData } from 'src/app/models/projects';
 import { ProjectsHasUser } from '../../models/projectsHasUser';
 import { UpdateDataService } from 'src/app/services/update-data.service';
+import { UsersListService } from '../../services/users-list.service';
+import { UserData } from '../../models/users';
 
 @Component({
   selector: 'app-project-members',
@@ -14,7 +17,14 @@ export class ProjectMembersComponent implements OnInit {
   @Input() project:ProjectData = new ProjectData();
 
   members: ProjectsHasUser[] = [];
-  constructor(public memberList:UsersProjectsService, private emitter: UpdateDataService) {
+  form: FormGroup = new FormGroup({
+    userName: new FormControl(),
+    userRol: new FormControl(1)
+  });
+
+  memberData: UserData[] = [];
+  newMember: ProjectsHasUser = new ProjectsHasUser();
+  constructor(public memberList:UsersProjectsService, private emitter: UpdateDataService, private userList: UsersListService) {
     this.emitter.emitter.subscribe(() => {
       this.reloadMembers();
     });
@@ -31,5 +41,21 @@ export class ProjectMembersComponent implements OnInit {
     this.members = this.memberList.projectMembers.filter(memberList=> {
       return memberList.proyects_id_p == this.project.id
     })
+  }
+
+  addMember() {
+    this.memberData = this.userList.usersList.filter(data => { return data.userName == this.form.get('userName')?.value});
+    if (this.memberData != undefined) {
+      this.newMember.proyects_id_p = this.project.id;
+      this.newMember.user_id_user = this.memberData[0].userId;
+      this.newMember.roles_id_rol = this.form.get('userRol')?.value;
+
+      this.memberList.projectMembers.push(this.newMember);
+    }
+
+    this.form = new FormGroup({
+      userName: new FormControl(),
+      userRol: new FormControl(1)
+    });
   }
 }
